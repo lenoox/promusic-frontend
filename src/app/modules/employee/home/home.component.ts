af9631728a4eaf05b90e513662cf00187417dbaa
lenoox <62141973+lenoox@users.noreply.github.com>
2021-04-24 23:03:39 +0200

    added home page employee user

---
+++ src/app/modules/employee/home/home.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {Order} from '../../../shared/model/order';
+import {OrderService} from '../../../core/service/order.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  ordersCount: number;
+
+  constructor(private orderService: OrderService) {
+  }
+  ngOnInit(): void {
+    this.loadProduct();
+  }
+  loadProduct(): void{
+    this.orderService.GetOrders().subscribe((data: Order[]) => {
+      this.ordersCount = data.length;
+    });
+  }
+}
