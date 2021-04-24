824d9fece7c374678eb5e9612c9caea1de551d66
lenoox <62141973+lenoox@users.noreply.github.com>
2021-04-24 23:17:59 +0200

    refactored shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,12 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    NgbModule,
-    HttpClientModule
+    NgbModule
   ],
   exports: [
     NgbModule
