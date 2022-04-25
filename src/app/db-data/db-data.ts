d4c06b082d41bbc9805bc1e75ac3759c1bb774d3
lenoox <62141973+lenoox@users.noreply.github.com>
2022-04-25 22:54:19 +0200

    fix unit test

---
+++ src/app/db-data/db-data.ts
@@ -0,0 +1,22 @@
+export const USER_INFO: any = {
+  "id": 2,
+  "firstName": "Jan",
+  "lastName": "Kowalski",
+  "username": "jan.kowalski@wp.pl",
+  "address": "ul. Ogrodowa  12/8C 10-001",
+  "phoneNumber": "001-002-143",
+  "city": "Warszawa",
+  "active": true,
+  "role": {"id": 1, "name": "USER"}
+};
+export const EMPLOYEE_INFO: any = {
+  "id": 2,
+  "firstName": "Jan",
+  "lastName": "Nowak",
+  "username": "jan.kowalski@wp.pl",
+  "address": "ul. Ogrodowa  12/8C 10-001",
+  "phoneNumber": "001-002-143",
+  "city": "Warszawa",
+  "active": true,
+  "role": {"id": 2, "name": "EMPLOYEE"}
+};
